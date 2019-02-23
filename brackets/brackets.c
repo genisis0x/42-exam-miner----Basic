928a5e071383aaceffbb625225f39a756af8189c
Manmeet <manietdavv@gmail.com>
2019-02-23 01:47:02 -0800

    Done

---
+++ brackets/brackets.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brackets.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/15 10:15:17 by maparmar          #+#    #+#             */
+/*   Updated: 2019/02/23 01:43:33 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+# define BUFF_SIZE 2048
+
+int is_pair(char c, char d)
+{
+	return ((c == '(' && d == ')') || (c == '{' && d == '}') || (c == '[' && d == ']'));
+}
+
+int ft_brackets (char *str)
+{
+	int j = 0;
+	int stack[2048];
+	int position = 0;
+	while (str[j])
+	{
+		if (str[j]== '(' || str[j] == '{' || str[j] == '[')
+			stack[++position] = str[j];
+		if (str[j]== ')' || str[j] == '}' || str[j] == ']')
+			if (!(is_pair(stack[position--], str[j])))
+				return (0);
+		j++;
+	}
+	return (!position);
+}
+
+int main (int ac, char **av)
+{
+	int i = 1;
+
+	if (ac == 1)
+	{
+		write (1, "\n", 1);
+		return (0);
+	}
+	while (ac > i)
+	{
+		if (ft_brackets(av[i]))
+		{
+			write (1, "OK\n", 3);
+		}
+		else
+			write (1, "Error\n", 6);
+		i++;
+	}
+	return (0);
+}
+
+
---
+++ ft_split/ft_split.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/22 23:11:43 by maparmar          #+#    #+#             */
+/*   Updated: 2019/02/23 00:50:04 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int ft_isspace(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+int no_words(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		if (ft_isspace(*s))
+		{
+			s++;
+		}
+		else
+		{
+			++len;
+			while (*s && !ft_isspace(*s))
+			{
+				s++;
+			}
+		}
+	}
+	return (len);
+}
+
+
+char    **ft_split(char *str)
+{
+	char **res;
+	int word_len = 0;
+	int i = 0;
+	int start;
+	int k;
+	int j = 0;
+	int words = no_words(str);
+	res = (char**)malloc(sizeof(char*) * (words + 1));
+	while (str[i] && j < words)
+	{
+		while (str[i] && ft_isspace(str[i]))
+		{
+			i++;
+		}
+		start = i;
+		while (str[i] && !ft_isspace(str[i]))
+		{
+			word_len++;
+			i++;
+		}
+		res[j] = (char*)malloc(sizeof(char) * (word_len + 1));
+		k = 0;
+		while (word_len)
+		{
+			res[j][k] = str[start];
+			word_len--;
+			start++;
+			k++;
+		}
+		res[j][k] = '\0';
+		j++;
+	}
+	res[j] = 0;
+	return (res);
+}
+int main (void)
+{
+	char** res;
+	for (res = ft_split("asdf qwerty zxcv"); *res != '\0'; res++)
+		printf("'%s',", *res);
+	printf("\n");
+	return (0);
+}
