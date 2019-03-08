1e85207e7fdc852279ee0b1781e74876d8a6dbcb
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-08 01:00:17 -0800

    Update is_power_of_2.c

--- is_power_of_2/is_power_of_2.c
+++ is_power_of_2/is_power_of_2.c
@@ -15,5 +15,5 @@ int	    is_power_of_2(unsigned int n)
 	if (n == 0)
 		return (0);
 	else
-		return ((n & (-n)) == n); // 2's Complement of the number is (-n) and if the AND logic of nbr and it's 2's complement is number itself then its power of two
+		return ((n & (-n)) == n); // 2's Complement of the number is (-n) and if the AND logic of nbr and it's 2's complement is number itself then its power of 2
 }
