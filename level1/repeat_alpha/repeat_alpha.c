4d4f23e49c7d8e8c1e1c1c97259b5230465eb516
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-10 03:52:00 -0700

    Comment Updated

--- level1/repeat_alpha/repeat_alpha.c
+++ level1/repeat_alpha/repeat_alpha.c
@@ -15,26 +15,26 @@
 int main (int ac, char **av)
 {
 	int i = 0;
-	int rep;
+	int rep; // counter to count the alpha position in ascii table
 	char c;
 	if (ac == 2)
 	{
 		while (av[1][i])
 		{
 			c = av[1][i]; 
-			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) // if it's a alpha
 			{
 				if (c >= 'a' && c <= 'z')
 					rep = c - 'a';
 				if (c >= 'A' && c <= 'Z')
 					rep = c - 'A';
-				while (rep > 0)
+				while (rep > 0) // when the counter is greater then 0 then write it
 				{
 					write (1, &c, 1);
 					rep--;
 				}
 			}
-			write (1, &c, 1);
+			write (1, &c, 1); // anyways write each word for one time.
 			i++;
 		}
 	}
