359cea50aaaea976e3317249bd8c29c332f9a11f
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-11 01:44:16 -0700

    Create str_capitalizer.c

---
+++ level3/str_capitalizer.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_capitalizer.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/26 00:55:51 by maparmar          #+#    #+#             */
+/*   Updated: 2019/02/26 00:57:47 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	str_capitalizer(char *str)
+{
+	int		i;
+
+	i = 0;
+	if (str[i] >= 'a' && 'z' >= str[i])
+		str[i] -= 32;
+	write(1, &str[i], 1);
+	while (str[++i])
+	{
+		if (str[i] >= 'A' && 'Z' >= str[i])
+			str[i] += 32;
+		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || \
+		str[i] == '\t'))
+			str[i] -= 32;
+		write(1, &str[i], 1);
+	}
+}
+
+int		main(int argc, char *argv[])
+{
+	int		i;
+
+	if (argc <= 1)
+		write(1, "\n", 1);
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			str_capitalizer(argv[i]);
+			write(1, "\n", 1);
+			i += 1;
+		}
+	}
+	return (0);
+}
