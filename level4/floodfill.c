d8c0d071779ca5b27b59d704cea97d730d6359d2
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-11 20:48:02 -0700

    Update floodfill.c

--- level4/floodfill.c
+++ level4/floodfill.c
@@ -1,21 +1,22 @@
 void  flood_fill(char **tab, t_point size, t_point begin)
 {
-	char c = tab[begin.y][begin.x];
+	char c;
+	c = tab[begin.y][begin.x];
 
 	tab[begin.y][begin.x] = 'F';
-    begin.y++;
-	if (begin.y < size.y && tab[begin.y][begin.x] == c)
+	begin.y++;
+	if (begin.y < size.y && tab[begin.y][begin.x] == c) // when the position + 1 in Y Direction is the given character
 		flood_fill(tab, size, begin);
 	begin.y--;
-    begin.y--;
-	if (begin.y >= 0 && tab[begin.y][begin.x] == c)
+	begin.y--;
+	if (begin.y >= 0 && tab[begin.y][begin.x] == c) // when the position - 1 in Y Direction is the given character
 		flood_fill(tab, size, begin);
-    begin.y++;
-    begin.x++;
-	if (begin.x < size.x && tab[begin.y][begin.x] == c)
+	begin.y++;
+	begin.x++;
+	if (begin.x < size.x && tab[begin.y][begin.x] == c) //when the position +1 in X Direction is the given character;
 		flood_fill(tab, size, begin);
-    begin.x--;
-    begin.x--;
-	if (begin.x >= 0 && tab[begin.y][begin.x] == c)
+	begin.x--;
+	begin.x--;
+	if (begin.x >= 0 && tab[begin.y][begin.x] == c) // when the position - 1 in X Direction is the given character;
 		flood_fill(tab, size, begin);
 }
