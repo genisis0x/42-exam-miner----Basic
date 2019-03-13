44c60eb5ad6fb22bfd6fa9aba3959389c45327bd
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-12 19:20:00 -0700

    Update ft_list_remove_if_shorter.c

--- level4/ft_list_remove_if/ft_list_remove_if_shorter.c
+++ level4/ft_list_remove_if/ft_list_remove_if_shorter.c
@@ -18,22 +18,22 @@ void 	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 	t_list	*tmp;
 	t_list	*i;
 
-	while (*begin_list && !cmp((*begin_list)->data, data_ref))
+	while (*begin_list && !cmp((*begin_list)->data, data_ref)) // if the match element is in the starting of the list then delete untill you got the element which doen't match.
 	{
 		tmp = *begin_list;
 		*begin_list = (*begin_list)->next;
 		free(tmp);
 	}
 	i = *begin_list;
-	while (i && i->next)
+	while (i && i->next) // if the match element is not in the starting but at any arbitary part of the list.
 	{
-		if (!cmp(i->next->data, data_ref))
+		if (!cmp(i->next->data, data_ref)) // if it matches just delete the node and deleting the node means you are automatically getting shiftted by one.
 		{
 			tmp = i->next;
 			i->next = tmp->next;
 			free(tmp);
 		}
-		else if (i->next)
+		else if (i->next) // if the element doen't matches then just move forward by one if the next value is not null. 
 			i = i->next;
 	}
 }
