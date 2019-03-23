6784d6ef6a5c337902eda219f50100553d2b2189
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-22 22:16:24 -0700

    Comments Updated

--- level4/ft_list_remove_if/ft_list_remove_if.c
+++ level4/ft_list_remove_if/ft_list_remove_if.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 
+/**********solution is not good approch but it work's, for good approch see other *.c file***********/
+
 #include "ft_list.h"
 #include <stdlib.h>
 void 	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
@@ -20,27 +22,27 @@ void 	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 
 	if (*begin_list == NULL)
 		return ;
-	while(*begin_list && (!cmp((*begin_list)->data, data_ref)))
+	while(*begin_list && (!cmp((*begin_list)->data, data_ref))) // delete the element which is in the starting of the list.
 	{
 		temp = *begin_list;
 		*begin_list = (*begin_list)->next;
 		free (temp);
 	}
 	traverse = *begin_list;
-	while (traverse && traverse->next)
+	while (traverse && traverse->next) // loop used to delete the element in the middle or arbitary position other than starting.
 	{
-		if (!cmp(traverse->next->data, data_ref))
+		if (!cmp(traverse->next->data, data_ref)) // if it's not sorted.
 		{
 			temp = traverse->next;
 			traverse->next = temp->next;
 			free(temp);
 		}
-		if (traverse->next->next)
+		if (traverse->next->next) // if next to next exist.
 			traverse = traverse->next;
 		else
 			break;
 	}
-	if (traverse->next && traverse->next->next ==  NULL)
+	if (traverse->next && traverse->next->next ==  NULL) // remove the last element.
 	{
 		if (!(cmp(traverse->next->data, data_ref)))
 		{
