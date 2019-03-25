f83bc9e7730b3a44a68c2c4a9d0d65782d35beb3
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-24 19:15:44 -0700

    Create rev_print_iterative.c

---
+++ level1/rev_print/rev_print_iterative.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i;
+
+		i = 0;
+		while(argv[1][i])
+			i += 1;
+		while(i)
+			write(1, &argv[1][--i], 1);
+	}
+	write(1 ,"\n", 1);
+	return (0);
+}
