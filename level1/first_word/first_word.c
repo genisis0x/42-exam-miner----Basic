5dd0d671ad68dfd280f3afa156beefe4a920fa81
Manmeet <manietdavv@gmail.com>
2019-03-25 01:23:24 -0700

    Level 0 and Level 1

---
+++ level1/first_word/first_word.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/25 01:14:13 by maparmar          #+#    #+#             */
+/*   Updated: 2019/03/25 01:14:16 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_isspace(int i)
+{
+	if (i == '\t' || i == '\n' || i == '\v' || i == '\f' || i == ' ')
+		return (1);
+	return (0);
+}
+
+int		main(int argc, char *argv[])
+{
+	int		i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (ft_isspace((argv[1][i])))
+				i += 1;
+		while (!(ft_isspace(argv[1][i])) && argv[1][i])
+			write(1, &argv[1][i++], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
