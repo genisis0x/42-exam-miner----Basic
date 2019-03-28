e37386d7e85dc90dbc9b98266c981289dbcebabf
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-27 21:28:41 -0700

    Updated correct one
    we are not allowed to use malloc so rewrite the whole thing just by understanding the string concept

--- level3/epur_str/epur_str.c
+++ level3/epur_str/epur_str.c
@@ -10,78 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <unistd.h>
 
-int		ft_strlen(char *str)
+int		main(int ac, char const **av)
 {
 	int		i;
+	int		flag = 0;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*trim_begin_end_space(char *str)
-{
-	char	*s;
-	int		i;
-	int		j;
-	int		k;
-
-	i = 0;
-	k = 0;
-	j = ft_strlen(str);
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	while (str[j - 1] == ' ' || str[j - 1] == '\t')
-		j--;
-	s = (char*)malloc(sizeof(char) * (j - i + 1));
-	if (s == NULL)
-		return (NULL);
-	while (k < j - i)
+	if (ac == 2)
 	{
-		s[k] = str[i + k];
-		k++;
-	}
-	s[k] = '\0';
-	return (s);
-}
-
-void	epur_str(char *str)
-{
-	int		i;
-	int		first;
-
-	i = 0;
-	first = 1;
-	str = trim_begin_end_space(str);
-	if (str != NULL)
-	{
-		while (str[i])
+		i = 0;
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i += 1;
+		while (av[1][i])
 		{
-			if (str[i] == ' ' || str[i] == '\t')
+			if (av[1][i] == ' ' || av[1][i] == '\t')
+				flag = 1;
+			if (!(av[1][i] == ' ' || av[1][i] == '\t'))
 			{
-				if (first == 1)
-					write(1, &str[i], 1);
-				first = 0;
-			}
-			else
-			{
-				first = 1;
-				write(1, &str[i], 1);
+				if (flg)
+					write(1, " ", 1);
+				flg = 0;
+				write(1, &av[1][i], 1);
 			}
 			i++;
 		}
 	}
-	free(str);
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc > 1)
-		epur_str(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
