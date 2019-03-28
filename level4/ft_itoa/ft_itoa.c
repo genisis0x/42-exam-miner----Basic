edbcbd7b509a1f8033510e812de00df587dd0e93
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-03-27 21:42:00 -0700

    Update ft_itoa.c

--- level4/ft_itoa/ft_itoa.c
+++ level4/ft_itoa/ft_itoa.c
@@ -18,20 +18,20 @@ char	*ft_itoa(int nbr)
 
 	str = "0123456789"; // base str
 	len = nbr < 0 ? 1 : 0; // increase len by one for -neg
-	nb = nbr < 0 ? -(unsigned int )nbr : nbr; //convert to unsigned int for -neg
+	nb = nbr < 0 ? -(unsigned int )nbr : nbr; //convert to unsigned int for -neg to take care of INT_MIN
 	len = ft_intlen(nb, len); // length of int
-	len = (nbr == 0) ? 1 : len; // for 0 reset the len to 0 
-	if (!(res = (char *)malloc(sizeof(char) * (len + 1)))) // initilize the dynamic memory
+	len = (nbr == 0) ? 1 : len; // for 0 reset the len to 1 
+	if (!(res = (char *)malloc(sizeof(char) * (len + 1)))) // initilize the dynamic memory in cpu.
 		return (NULL);
 	if (nbr == 0)
 		res[0] = '0';
-	res[len] = '\0'; // null termination
+	res[len] = '\0'; // null termination to end the string.
 	while (nb > 0) // assign all the values from back
 	{
 		res[--len] = str[nb % 10];
 		nb /= 10;
 	}
-	if (nbr < 0) // if -neg assign '-' in starting
+	if (nbr < 0) // if nbr is -neg assign '-' in starting.
 		res[0] = '-';
 	return (res);
 }
