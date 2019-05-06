9d693cb2e7e52549514de6820fdf0f72b00e3b65
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-05-05 22:51:55 -0700

    Create fprime2.c

---
+++ level4/fprime/fprime2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fprime(int a)
+{
+    int flag = 0;
+    if(a == 1)
+    {
+        printf("1");
+        return;
+    }
+    while(a % 2 == 0)
+    {
+        if(flag > 0)
+            printf("*");
+        printf("2");
+        a /= 2;
+        flag ++;
+    }
+    for (int i = 3; i *i <= a; i+=2)
+    {
+        
+        while(a % i == 0)
+        {
+            if(flag > 0)
+                printf("*");
+            a /= i;
+            printf("%d", i);
+            flag++;
+        }
+    }
+    if(a > 2)
+    {
+        if(flag > 0)
+            printf("*");
+        printf("%d", a);
+    }
+}
+
+int main(int ac, char **av)
+{
+    if(ac ==2)
+    {
+        fprime(atoi(av[1]));
+    }
+    printf("\n");
+    return 0;
+}
