cf7dcfe7edff5319ee2cf0f829f3f224b18fbae1
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-07-01 15:05:42 +0530

    Update lcm_interative_mod_method.c

--- level3/lcm/lcm_interative_mod_method.c
+++ level3/lcm/lcm_interative_mod_method.c
@@ -1,17 +1,19 @@
 #define max(a, b) (a > b ? a : b)
 #define min(a, b) (a > b ? b : a)
+
 unsigned int gcd(unsigned int a, unsigned int b)
 {
-	    int k = max(a,b);
-      int m = min(a,b);
-      // loop invariant: k ≥ m ∧ GCD(a,b) = GCD(k,m)
-      while (m != 0) {
-         int r = k % m;
-         k = m;
-         m = r;
-      }
-      // At this point, GCD(a,b) = GCD(k,m) = GCD(k,0) = k
-      return k;
+	int k = max(a,b);
+	int m = min(a,b);
+	// loop invariant: k ≥ m ∧ GCD(a,b) = GCD(k,m)
+	while (m != 0)
+	{
+		int r = k % m;
+		k = m;
+		m = r;
+	}
+  // At this point, GCD(a,b) = GCD(k,m) = GCD(k,0) = k
+  return k;
 }
 
 unsigned int	lcm(unsigned int a, unsigned int b)
@@ -22,15 +24,13 @@ unsigned int	lcm(unsigned int a, unsigned int b)
 
 // TEST MAIN
 
-/*
-#include <stdio.h>
-#include <stdlib.h>
-int main(int ac, char **av)
-{
-	if(ac >= 2)
-	{
-		printf("%d", lcm(atoi(av[1]), atoi(av[2])));
-}
-	return 0;
-}
-*/
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(int ac, char **av)
+// {
+// 	if(ac >= 2)
+// 	{
+// 		printf("%d", gcd(atoi(av[1]), atoi(av[2])));
+// }
+// 	return 0;
+// }
