d5ba00cdc3edfdc6f09eb6b23e979bcc3f1d012a
Manmeet2018 <manietdavv@gmail.com>
2019-08-16 23:54:17 +0530

    ft_swap_toggling_bit_wise

--- level1/ft_swap/ft_swap1.c
+++ level1/ft_swap/ft_swap1.c
@@ -1,20 +1,18 @@
 void	ft_swap(int *a, int *b)
 {
-	*a ^= *b; // a = a xor b;
-	*b ^= *a; // b = (a xor b xor b) or (a);
-	*a ^= *b; // a = (a xor b xor a) or(b);
+	*a ^= *b ^= *a ^= *b; // XOR operation using toggling method
 }
 
 
 // XOR of the element with the element gives 0;
 
-/*#include <stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a = 12;
-    int b = 13;
+    int a = 13;
+    int b = 12;
     ft_swap(&a, &b);
     printf("The value of a is %d && b is %d", a, b);
     return 0;
-}*/
+}
 
