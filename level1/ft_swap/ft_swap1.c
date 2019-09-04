a8015dae5dbe844455a2067fbac65c9bdee8ae46
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-09-05 01:29:13 +0530

    Update ft_swap1.c

--- level1/ft_swap/ft_swap1.c
+++ level1/ft_swap/ft_swap1.c
@@ -1,3 +1,4 @@
+//PASSED IN EXAM
 void	ft_swap(int *a, int *b)
 {
 	*a ^= *b ^= *a ^= *b; // XOR operation using toggling method
@@ -6,13 +7,13 @@ void	ft_swap(int *a, int *b)
 
 // XOR of the element with the element gives 0;
 
-#include <stdio.h>
-int main()
-{
-    int a = 13;
-    int b = 12;
-    ft_swap(&a, &b);
-    printf("The value of a is %d && b is %d", a, b);
-    return 0;
-}
+// #include <stdio.h>
+// int main()
+// {
+//     int a = 13;
+//     int b = 12;
+//     ft_swap(&a, &b);
+//     printf("The value of a is %d && b is %d", a, b);
+//     return 0;
+// }
 
